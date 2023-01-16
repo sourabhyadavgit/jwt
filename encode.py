9c9cc89fa0367a47a98110793894b3c01f3d5ecf
Sourabh Yadav <saurav_sori@yahoo.com>
2023-01-16 12:40:46 +0000

    jwt test

---
+++ encode.py
@@ -0,0 +1,27 @@
+import jwt
+
+
+def encode_user():
+    encoded_data = jwt.encode(payload={"user": "sys-test-admin", "access": " approved"},
+                              key='sy-secret',
+                              algorithm="HS256")
+                    
+    return encoded_data
+
+def decode_user(token: str):
+    """
+    :param token: jwt token
+    :return:
+    """
+    decoded_data = jwt.decode(jwt=token,
+                              key='sy-secret',
+                              algorithms=["HS256"])
+
+    return decoded_data
+
+if __name__ == "__main__":
+    token = encode_user()
+    decoder = decode_user(token)
+    print(encode_user())
+    print(" decoded value is : ")
+    print(decoder)
